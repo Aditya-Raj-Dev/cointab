264b6eb2ad3b3d0ca455bed45f5859526a5701c4
Aditya Raj <aadiyadav56837492@gmail.com>
2023-02-09 16:03:01 +0530

    k

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-
+mongoose.set('strictQuery', true);
 const connection = mongoose.connect('mongodb+srv://adi:123@cluster0.s2zqiy6.mongodb.net/test')
 
 module.exports={connection}
